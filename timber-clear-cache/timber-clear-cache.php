7d554dc4a89e76bb5714cb108f48d201da9865bf
Ondrej Grosko <ogrosko@gmail.com>
2015-04-23 10:48:35 +0200

    ajax loader

--- timber-clear-cache/timber-clear-cache.php
+++ timber-clear-cache/timber-clear-cache.php
@@ -10,14 +10,6 @@ Network: True
 Text Domain: timber-clear-cache
 */
 
-//check if Timber is installed
-if ( ! class_exists('Timber') ) {
-	add_action( 'admin_notices', function() {
-		echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php' ) ) . '</a></p></div>';
-	} );
-	return;
-}
-
 //add button to admin menu bar
 function add_timber_clear_cache_admin_button() {
     global $wp_admin_bar;
@@ -26,42 +18,58 @@ function add_timber_clear_cache_admin_button() {
         return;
     
     $wp_admin_bar->add_menu(array(
-        'id' => 'clear_timber_cache',
+        'id' => 'clear-timber-cache',
         'title' => __( 'Clear Timber Cache'),
         'href' => '#',
         'meta' => array(
-        	'onclick' => 'clear_timer_cache()'
+        	'html' => '<div class="spinner"></div>',
+        	'onclick' => 'clear_timber_cache(jQuery(this))'
         )
     ));
 }
-add_action('admin_bar_menu', 'add_timber_clear_cache_admin_button');
+add_action('admin_bar_menu', 'add_timber_clear_cache_admin_button', 110);
 
-//add footer script ajax posting
+/**
+ * Add footer script ajax posting
+ */
 add_action( 'admin_footer', 'clear_timer_cache_javascript' ); // Write our JS below here
 function clear_timer_cache_javascript() { ?>
 	<script type="text/javascript" >
-		function clear_timer_cache() {
+		function clear_timber_cache(button) {
+			jQuery('li#wp-admin-bar-clear-timber-cache .spinner').addClass('active');
 			var data = {
-				'action': 'my_action'
+				'action': 'clear_timber_cache_action'
 			};
 
 			jQuery.post(ajaxurl, data, function(response) {
-				alert(response);
+				jQuery('li#wp-admin-bar-clear-timber-cache .spinner').removeClass('active');
 			});
 		}
 	</script> <?php
 }
 
-//ajax script
-add_action( 'wp_ajax_my_action', 'my_action_callback' );
-function my_action_callback() {
-	$temDir = plugins_url().'/timber-library/cache/twig';
-	if (file_exists($temDir)) {
-		rmdir($temDir);
+/**
+ * PHP ajax script
+ */
+add_action( 'wp_ajax_clear_timber_cache_action', 'clear_timber_cache_callback' );
+function clear_timber_cache_callback() {
+	$tempDir = plugins_url().'/timber-library/cache/twig';
+
+	if (file_exists($tempDir)) {
+		rmdir($tempDir);
 	}
+	
 	TimberCommand::clear_cache();
 
     echo 'Timber cache cleared successfully';
 
 	wp_die();
+}
+
+/**
+ * Add custom styles
+ */
+add_action('admin_head', 'clear_timber_cache_style');
+function clear_timber_cache_style() {
+	wp_enqueue_style( 'clear-timber-cache-style',  plugins_url().'/timber-clear-cache/style.css' );
 }
