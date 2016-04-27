b69c2b316a1c83b81f8ac5445a2e9c761f3eee77
Ondrej Grosko <ogrosko@gmail.com>
2016-04-27 15:41:47 +0200

    Fixes:
      - Fixed bug with clearing cache from FE admin bar
    
    Other:
    
      - Added markdown README for Github (thanks to @DesignyourCode)
      - Loader style finetuning

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,11 @@
-function clear_timber_cache(button) {
-	jQuery('#wp-admin-bar-clear-timber-cache .spinner').addClass('active');
-	var data = {
-		'action': 'clear_timber_cache_action'
-	};
+function clear_timber_cache($button) {
+	jQuery('#wp-admin-bar-clear-timber-cache .loader').addClass('active');
+	var ajaxUrl = $button.attr('href'),
+		data = {
+			'action': 'clear_timber_cache_action'
+		};
 
-	jQuery.post(ajaxurl, data, function(response) {
-		jQuery('#wp-admin-bar-clear-timber-cache .spinner').removeClass('active');
+	jQuery.post(ajaxUrl, data, function(response) {
+		jQuery('#wp-admin-bar-clear-timber-cache .loader').removeClass('active');
 	});
 }
--- clear-cache-for-timber.php
+++ clear-cache-for-timber.php
@@ -4,53 +4,49 @@ Plugin Name: Clear cache for Timber
 Plugin URI: https://github.com/ogrosko/timber-clear-cache
 Description: Clear cache for Timber and Twig caching
 Author: Ondrej Grosko
-Version: 0.0.4
+Version: 0.0.5
 Author URI: 
 Network: True
 Text Domain: clear-cache-for-timber
 */
 
-//add button to admin menu bar
+/**
+* Add button to admin menu bar
+*/
+add_action('admin_bar_menu', 'add_timber_clear_cache_admin_button', 110);
 function add_timber_clear_cache_admin_button() {
     global $wp_admin_bar;
 
-    if ( !is_super_admin() || !is_admin_bar_showing() || !class_exists('Timber') )
-        return;
+    if ( !is_super_admin() || !is_admin_bar_showing() || !class_exists('Timber') || !\Timber::$cache ) { 
+          return;
+    }
     
     $wp_admin_bar->add_menu(array(
         'id' => 'clear-timber-cache',
         'title' => __( 'Clear Timber Cache'),
-        'href' => '#',
+        'href' => admin_url('admin-ajax.php'),
         'meta' => array(
-        	'html' => '<div class="spinner"></div>',
-        	'onclick' => 'clear_timber_cache(jQuery(this))'
+            'html' => '<img src="'.plugins_url('assets/images/loader.svg', __FILE__).'" class="loader" alt="clear timber cache loader" />',
+            'onclick' => 'clear_timber_cache(jQuery(this)); return false;'
         )
     ));
 }
-add_action('admin_bar_menu', 'add_timber_clear_cache_admin_button', 110);
-
 
 /**
  * PHP ajax script
  */
 add_action( 'wp_ajax_clear_timber_cache_action', 'clear_timber_cache_callback' );
 function clear_timber_cache_callback() {
-	echo TimberCommand::clear_cache();
-	wp_die();
+    echo TimberCommand::clear_cache();
+    wp_die();
 }
 
 /**
- * Add plugin scripts
+ * Enqueue resources
  */
-add_action( 'admin_footer', 'clear_timer_cache_javascript' );
+add_action( 'admin_enqueue_scripts', 'clear_timer_cache_javascript' );
+add_action( 'admin_bar_init', 'clear_timer_cache_javascript' );
 function clear_timer_cache_javascript() { 
-	wp_enqueue_script('clear-cache-for-timber-javascript', plugins_url('assets/js/main.js', __FILE__), array(), '0.0.4', true);
-}
-
-/**
- * Add plugin styles
- */
-add_action('admin_head', 'clear_timber_cache_style');
-function clear_timber_cache_style() {
-	wp_enqueue_style( 'clear-cache-for-timber-style',  plugins_url('assets/css/style.css', __FILE__), array(), '0.0.4' );
+    wp_enqueue_script('clear-cache-for-timber-javascript', plugins_url('assets/js/main.js', __FILE__), array(), '0.0.5', true);
+    wp_enqueue_style( 'clear-cache-for-timber-style',  plugins_url('assets/css/style.css', __FILE__), array(), '0.0.5' );
 }
